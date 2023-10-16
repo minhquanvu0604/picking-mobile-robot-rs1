e3f021ab74974728cd4a9ca3fb54e101c440f6c0
anhtungnguyen <anhtungnguyen1809@gmail.com>
2023-10-16 15:58:05 +1100

    get picking list and send goal done

--- picking_mobile_robot_rs1_control/src/client.py
+++ picking_mobile_robot_rs1_control/src/client.py
@@ -14,9 +14,12 @@ import actionlib
 from std_srvs.srv import Trigger, TriggerResponse
 from collections import deque
 from std_msgs.msg import String
+from std_msgs.msg import Float32
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
-
+#NEED TO ADD PUBLISHER TO CONSUMER TOPIC (/BATTERY/CONSUMER/0) AT 9 AMPS WHILE ROBOT IS MOVING. 
+#NEED TO ADD SERVICE CALL TO BATTERY CHARGE TO TOP UP BATTERY WHEN AT HOME STATION
+#NEED TO ADD BATTERY CHECKER BEFORE MOVING TO RETURN TO HOME BEFORE MOVING. 
 class CsvReaderNode:
     def __init__(self):
 
@@ -25,6 +28,13 @@ class CsvReaderNode:
 
         # Initialize the ROS node
         rospy.init_node('mission_handler')
+
+        # Subscriber:
+        self.battery_check = rospy.Subscriber('/battery_voltage_topic', Float32, self.battery_callback)
+        self.battery_voltage = None
+
+        # Voltage publisher (change it to service)
+        self.voltage_publisher = rospy.Publisher('publish_voltage', Float32, queue_size=1)
 
         # Get package path
         rospack = rospkg.RosPack()
@@ -39,12 +49,54 @@ class CsvReaderNode:
 
         # Create an action client to communicate with move_base
         self.move_base_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+
         rospy.loginfo("Waiting for move_base action server...")
         self.move_base_client.wait_for_server()
+
         rospy.loginfo("Connected to move_base action server")
 
-        # Sequence ????
-        #self.seq = 0
+    
+    def battery_callback(self, msg):
+        self.battery_voltage = msg
+
+    # change to service call
+    def reset_voltage(self):
+        self.battery_voltage = 11.1
+        self.voltage_publisher.publish(self.battery_voltage)
+        return None
+
+    def battery_state_check(self):
+        voltage = self.battery_voltage
+        
+        if voltage < 9.5:
+            rospy.loginfo("LOW BATTERY, GOING BACK TO CHARGE STATION")
+
+            goal_msg = MoveBaseGoal()
+            goal_msg.target_pose.header.frame_id = 'map'
+            goal_msg.target_pose.header.stamp = rospy.Time.now()
+
+            goal_msg.target_pose.pose.position.x = -6
+            goal_msg.target_pose.pose.position.y = 10
+            goal_msg.target_pose.pose.position.z = 0
+            goal_msg.target_pose.pose.orientation.x = 0
+            goal_msg.target_pose.pose.orientation.y = 0
+            goal_msg.target_pose.pose.orientation.z = 0
+            goal_msg.target_pose.pose.orientation.w = 1
+
+
+            rospy.loginfo("Sending next goal...")
+
+            # Publish the goal message
+            self.move_base_client.send_goal(goal_msg, done_cb=self.goal_done_cb)
+            
+            # Wait for result, turtlebot 
+            self.move_base_client.wait_for_result()
+
+            rospy.loginfo("CHARGING")
+            rospy.sleep(10)
+            # reset the voltage back to normal condition
+            self.reset_voltage
+
 
 
     def csv_callback(self, req):
@@ -54,7 +106,7 @@ class CsvReaderNode:
             with open(self.packing_list_path, 'r') as csv_file:
                 reader = csv.reader(csv_file)
                 for row in reader:
-                    product_number = self.get_number_from_name(row)
+                    product_number = self.get_number_from_name(row[0])
                     self.queue.append(product_number)
             # Clear the contents of the CSV
             with open(self.packing_list_path, 'w') as csv_file:
@@ -65,16 +117,44 @@ class CsvReaderNode:
 
             response.success = True
             response.message = "CSV file read successfully and data added to the queue."
-            self.queue_publisher.publish(self.queue)
+            # self.queue_publisher.publish(self.queue)
         except Exception as e:
             response.success = False
             response.message = "Failed to read the CSV file. Error: " + str(e)
+
+            import traceback
+            print(traceback.format_exc())
             return response
         
         # Add additional variable latter
         while (self.queue):
             # Send the first location in the list
             self.send_next_goal()
+        
+        if not self.queue:
+            goal_msg = MoveBaseGoal()
+            goal_msg.target_pose.header.frame_id = 'map'
+            goal_msg.target_pose.header.stamp = rospy.Time.now()
+
+            goal_msg.target_pose.pose.position.x = -6
+            goal_msg.target_pose.pose.position.y = 10
+            goal_msg.target_pose.pose.position.z = 0
+            goal_msg.target_pose.pose.orientation.x = 0
+            goal_msg.target_pose.pose.orientation.y = 0
+            goal_msg.target_pose.pose.orientation.z = 0
+            goal_msg.target_pose.pose.orientation.w = 1
+
+
+            rospy.loginfo("Backing starting position")
+
+            # Publish the goal message
+            self.move_base_client.send_goal(goal_msg)
+            
+            # Wait for result, turtlebot 
+            self.move_base_client.wait_for_result()
+
+            rospy.loginfo("FINISHING")
+
         return response
 
 
@@ -100,7 +180,8 @@ class CsvReaderNode:
             goal_y = 2
         
         goalPose = []
-        goalPose.append(goal_x, goal_y)
+        goalPose.append(goal_x)
+        goalPose.append(goal_y)
         return goalPose
     
 
@@ -108,8 +189,8 @@ class CsvReaderNode:
         with open(self.product_location_path, 'r') as csv_file:
             reader = csv.DictReader(csv_file)
             for row in reader:
-                if row[0] == fruit_name:
-                    return int(row[1])
+                if row['fruit_name'] == fruit_name:
+                    return int(row['quantity'])
         rospy.logwarn('This product is not available in the product list.')
         return None
 
@@ -133,7 +214,10 @@ class CsvReaderNode:
 
     def send_next_goal(self):
         if self.queue:
-            next_goal_number = self.goals_queue.popleft()
+            # Check the battery firt
+            # self.battery_state_check()
+
+            next_goal_number = self.queue.popleft()
             goal_pose = self.get_pose(next_goal_number)
 
             goal_msg = MoveBaseGoal()
@@ -152,10 +236,16 @@ class CsvReaderNode:
             rospy.loginfo("Sending next goal...")
 
             # Publish the goal message
-            self.move_base_client.send_goal(goal_msg, done_cb=self.goal_done_cb)
+            self.move_base_client.send_goal(goal_msg)
             
             # Wait for result, turtlebot 
             self.move_base_client.wait_for_result()
+
+            # Wait for placing product on turtlebot
+            rospy.loginfo("Waiting for placing product on turtlebot")
+            rospy.sleep(5.0)
+
+
 
 
 if __name__ == "__main__":
